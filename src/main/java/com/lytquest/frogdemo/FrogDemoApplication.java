f787552a9d72e0460b6319ddc7d67d32cac062e9
Akintomiwa Abolade <jboss@Akintomiwas-MacBook-Pro.local>
2022-03-28 19:42:40 +0100

    Project Startup

---
+++ src/main/java/com/lytquest/frogdemo/FrogDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lytquest.frogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lytquest/frogdemo/FrogDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lytquest.frogdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
