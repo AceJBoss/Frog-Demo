6c4105516639d5fed9287dbc5d720f54e4c5be9c
Akintomiwa Abolade <jboss@Akintomiwas-MacBook-Pro.local>
2022-03-28 20:44:40 +0100

    Service Implementation

--- src/main/java/com/lytquest/frogdemo/service/ExcelService.java
+++ src/main/java/com/lytquest/frogdemo/service/ExcelService.java
@@ -1,4 +1,11 @@
 package com.lytquest.frogdemo.service;
 
+import com.lytquest.frogdemo.entity.Book;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface ExcelService {
+    public void saveBook(MultipartFile file);
+    public List<Book> getAllBooks();
 }
--- src/main/java/com/lytquest/frogdemo/service/impl/ExcelServiceImpl.java
+++ src/main/java/com/lytquest/frogdemo/service/impl/ExcelServiceImpl.java
@@ -1,8 +1,35 @@
 package com.lytquest.frogdemo.service.impl;
 
+import com.lytquest.frogdemo.entity.Book;
+import com.lytquest.frogdemo.helper.ExcelHelper;
+import com.lytquest.frogdemo.repository.BookRepository;
 import com.lytquest.frogdemo.service.ExcelService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class ExcelServiceImpl implements ExcelService {
+    private BookRepository repository;
+    public ExcelServiceImpl(BookRepository repository){
+        this.repository = repository;
+    }
+    @Override
+    public void saveBook(MultipartFile file) {
+        try {
+            List<Book> tutorials = ExcelHelper.excelToBooks(file.getInputStream());
+            repository.saveAll(tutorials);
+        } catch (IOException e) {
+            throw new RuntimeException("fail to store excel data: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return repository.findAll();
+    }
+
+
 }
