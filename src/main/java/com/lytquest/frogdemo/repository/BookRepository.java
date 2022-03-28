4a6293d3732cde296a2474610ead0157050104e2
Akintomiwa Abolade <jboss@Akintomiwas-MacBook-Pro.local>
2022-03-28 20:03:51 +0100

    Apache POI dependency

---
+++ src/main/java/com/lytquest/frogdemo/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.lytquest.frogdemo.repository;
+
+import com.lytquest.frogdemo.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
