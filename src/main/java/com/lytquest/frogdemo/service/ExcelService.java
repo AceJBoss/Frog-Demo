d7986e9a9de535d8fb67e3d4e1939e65b44bd5a5
Akintomiwa Abolade <jboss@Akintomiwas-MacBook-Pro.local>
2022-03-28 19:48:31 +0100

    Service Layer Setup

---
+++ src/main/java/com/lytquest/frogdemo/service/ExcelService.java
@@ -0,0 +1,4 @@
+package com.lytquest.frogdemo.service;
+
+public interface ExcelService {
+}
---
+++ src/main/java/com/lytquest/frogdemo/service/impl/ExcelServiceImpl.java
@@ -0,0 +1,8 @@
+package com.lytquest.frogdemo.service.impl;
+
+import com.lytquest.frogdemo.service.ExcelService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExcelServiceImpl implements ExcelService {
+}
